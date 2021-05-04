986e77074e5fa87f0c69a7a67ae6dc3ed502d55b
Jarno Rantaharju <rantahar@gmail.com>
2021-05-04 14:27:52 +0300

    Create a ModelView for the user model and require login.
    Need to create login view...

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from users.models import User
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+class UserDetailView(DetailView):
+    model = User
