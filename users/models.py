10ec8ed353dfb99eadbc2baa4908fa99fda9a9d7
Jarno Rantaharju <rantahar@gmail.com>
2021-05-11 17:12:44 +0300

    Fetch authorization token and user id after receiving oauth2 key

--- users/models.py
+++ users/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     oauth2_token = models.CharField(max_length=20, null=True, blank=True)
+    polar_id = models.CharField(max_length=20, null=True, blank=True)
+    access_token = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
         return self.email
--- users/views.py
+++ users/views.py
@@ -1,3 +1,6 @@
+import requests
+import base64
+
 from django.views.generic import DetailView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
@@ -8,6 +11,7 @@ from django.urls import reverse_lazy
 
 from users.models import User
 from users.forms import UserRegisterForm
+from polar_auth.settings import polar_key, polar_secret
 
 
 @method_decorator(login_required, name='dispatch')
@@ -33,9 +37,43 @@ class AddAuthTokenView(RedirectView):
         user = self.request.user
         print(user)
         token = self.request.GET.get('code', '')
+        print(self.request.GET)
         print(token)
 
+        # user the received code to request an access token
+        auth = f'{polar_key}:{polar_secret}'
+        auth_bytes = auth.encode('ascii')
+        base64_bytes = base64.b64encode(auth_bytes)
+        base64_auth = base64_bytes.decode('ascii')
+        print(base64_auth)
+
+        import time
+        time.sleep(1)
+
+        headers = {
+          'Authorization': f'Basic {base64_auth}'
+        }
+        data = f"grant_type=authorization_code&code={token}"
+
+        token_response = requests.post('https://polarremote.com/v2/oauth2/token/',
+                          data=data,
+                          headers=headers
+                          ).json()
+
         user.oauth2_token = token
+        user.polar_id = token_response["x_user_id"]
+        user.access_token = token_response["access_token"]
         user.save()
 
         return reverse_lazy('home')
+
+
+@method_decorator(login_required, name='dispatch')
+class GetAuthenticationView(RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        url = "https://flow.polar.com/oauth2/authorization"
+        url += "?response_type=code"
+        url += "&scope=accesslink.read_all"
+        url += f"&client_id={polar_key}"
+        return url
