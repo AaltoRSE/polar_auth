404ef56d33bad239a70fd6581e1cfc3af3cc6f64
Jarno Rantaharju <rantahar@gmail.com>
2021-05-29 09:28:48 +0300

    Describe the polar_id field

--- users/models.py
+++ users/models.py
@@ -4,6 +4,10 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
+
+    # We use the polar_id to identify the user to the data server.
+    # This could, in principle, be identify the user, but we already
+    # store the email here in any case.
     polar_id = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
