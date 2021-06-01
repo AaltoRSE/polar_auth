7c7107b60e092c4b694c9bb831e8d3c5459f5e37
Jarno Rantaharju <jarno.rantaharju@aalto.fi>
2021-06-01 17:48:18 +0300

    Add user manager to enable adding superuser

--- users/models.py
+++ users/models.py
@@ -1,8 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 
-
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, is_admin=False, is_staff=False, is_active=True):
+        if not email:
+            raise ValueError("User email missing")
+        if not password:
+            raise ValueError("User pasword missing")
+
+        user = self.model(
+            email=self.normalize_email(email)
+        )
+        user.set_password(password)
+        user.admin = is_admin
+        user.staff = is_staff
+        user.active = is_active
+
+        # Consent always starts as False
+        user.consent = False
+        
+        user.save()
+        return user
+        
+    def create_superuser(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("User email missing")
+        if not password:
+            raise ValueError("User password missing")
+
+        user = self.model(
+            email=self.normalize_email(email)
+        )
+        user.set_password(password)
+        user.admin = True
+        user.staff = True
+        user.active = True
+        user.consent = False
+        
+        user.save()
+        return user
+
+
 class User(AbstractUser):
     # Remove the username as a separate field (we use email)
     username = None
@@ -25,6 +65,9 @@ class User(AbstractUser):
     # This could, in principle, be identify the user, but we already
     # store the email here in any case.
     polar_id = models.CharField(max_length=20, null=True, blank=True)
+
+    # Set the user manager
+    objects = UserManager()
 
     def __str__(self):
         return self.email
