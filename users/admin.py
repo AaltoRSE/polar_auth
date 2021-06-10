09fcc60588be524f54a43affb21b47313c649c58
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2021-06-10 10:26:17 +0300

    Formatting changes

--- users/admin.py
+++ users/admin.py
@@ -4,6 +4,7 @@ from django.contrib.auth.admin import UserAdmin
 from users.models import User
 from .models import Subscriber
 
+
 # Register your models here.
 class CustomUserAdmin(UserAdmin):
     model = User
@@ -11,12 +12,13 @@ class CustomUserAdmin(UserAdmin):
     list_filter = ('email', 'consent', 'privacy', 'is_active',)
     fieldsets = (
         (None, {'fields': ('email', 'consent', 'privacy', 'password')}),
-        ('Permissions', {'fields': ('is_superuser','is_active')}),
+        ('Permissions', {'fields': ('is_superuser', 'is_active')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'consent', 'privacy', 'password', 'is_superuser', 'is_active')}
+            'fields': ('email', 'consent', 'privacy', 'password',
+                       'is_superuser', 'is_active')}
          ),
     )
     search_fields = ('email',)
