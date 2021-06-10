b062481cf43bbfd7a820e5f213e25bf600cb2445
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2021-06-10 11:41:07 +0300

    Check that emails are aalto

--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,21 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 from users.models import User, Subscriber
 
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
+
+    def clean(self):
+        ''' Validate Aalto email addresses. '''
+        cleaned_data = super().clean()
+        email = cleaned_data.get("email")
+        if not email.endswith("@aalto.fi"):
+            raise ValidationError(
+                    "Please provide an Aalto email address."
+                )
 
     class Meta:
         model = User
@@ -23,7 +33,7 @@ class PrivacyForm(forms.ModelForm):
 class ConsentForm(forms.ModelForm):
 
     # Consent consists of multiple questions. Add each here.
-    widget = forms.CheckboxInput(attrs={'class':'form-check-input'})
+    widget = forms.CheckboxInput(attrs={'class': 'form-check-input'})
     field_1 = forms.BooleanField(required=True, widget=widget)
     field_2 = forms.BooleanField(required=True, widget=widget)
     field_3 = forms.BooleanField(required=True, widget=widget)
@@ -36,6 +46,8 @@ class ConsentForm(forms.ModelForm):
         fields = ['consent']
 
     def clean(self):
+        ''' If the form gets submitted, the user has consented to all the
+            items. We can just set consent to true. '''
         cleaned_data = super().clean()
         cleaned_data['consent'] = True
         return cleaned_data
@@ -43,6 +55,15 @@ class ConsentForm(forms.ModelForm):
 
 class SubscriptionForm(forms.ModelForm):
     email = forms.EmailField()
+
+    def clean(self):
+        ''' Validate Aalto email addresses. '''
+        cleaned_data = super().clean()
+        email = cleaned_data.get("email")
+        if not email.endswith("@aalto.fi"):
+            raise ValidationError(
+                    "Please provide an Aalto email address."
+                )
 
     class Meta:
         model = Subscriber
