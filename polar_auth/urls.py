27032fcfc02f9187bf085ad520e832d9f5c33b1b
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2021-06-10 12:33:00 +0300

    give name to logout url and redirect to the main page

--- polar_auth/urls.py
+++ polar_auth/urls.py
@@ -23,7 +23,7 @@ import polar_auth.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='logout')),
+    path('logout/', auth_views.LogoutView.as_view(next_page='main'), name='logout'),
     path('registration/', users.views.RegistrationView.as_view(), name='registration'),
     path('consent/', users.views.ConsentView.as_view(), name='consent'),
     path('consent-success/', users.views.ConsentView.as_view(), name='consent-success'),
