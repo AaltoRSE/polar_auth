ebc2854139aba40f153404f23a4dfbe1dcf859a7
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2021-06-17 12:51:54 +0300

    Add authorized to the admin view

--- users/admin.py
+++ users/admin.py
@@ -8,17 +8,17 @@ from .models import Subscriber
 # Register your models here.
 class CustomUserAdmin(UserAdmin):
     model = User
-    list_display = ('email', 'consent', 'privacy', 'first_survey_done', 'is_active')
-    list_filter = ('email', 'consent', 'privacy', 'first_survey_done', 'is_active')
+    list_display = ('email', 'consent', 'privacy', 'first_survey_done', 'authorized')
+    list_filter = ('email', 'consent', 'privacy', 'first_survey_done', 'authorized')
     fieldsets = (
         (None, {'fields': ('email', 'home_address', 'consent', 'privacy', 'first_survey_done', 'password', 'user_id')}),
-        ('Permissions', {'fields': ('is_superuser', 'is_active')}),
+        ('Permissions', {'fields': ('is_superuser', 'authorized')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
             'fields': ('email', 'home_address', 'consent', 'privacy', 'first_survey_done', 'password'
-                       'is_superuser', 'is_active')}
+                       'is_superuser', 'authorized')}
          ),
     )
     search_fields = ('email',)
