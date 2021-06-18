5e42533d5e8f269dd7c801b5db3563fa3d1ae610
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2021-06-18 13:50:01 +0300

    Add a password reset form

--- polar_auth/urls.py
+++ polar_auth/urls.py
@@ -22,6 +22,7 @@ import users.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='main'), name='logout'),
     path('registration/', users.views.RegistrationView.as_view(), name='registration'),
