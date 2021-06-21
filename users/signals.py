664fd2fc5a82a7f38eb80df1e5a6d209c61276f4
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2021-06-21 14:54:20 +0300

    Mark that the user has done the survey.

--- users/signals.py
+++ users/signals.py
@@ -6,5 +6,9 @@ from users.emails import send_enrolment_email
 @receiver(survey_completed)
 def check_enrolment(**kwargs):
     user = kwargs['user']
+    
+    user.first_survey_done = True
+    user.save()
+
     if user.ready_to_authorize():
         send_enrolment_email(user.email)
