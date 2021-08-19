ac60bb4c582509e3d65946c34ad85672f8254c53
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2021-08-19 14:14:46 +0300

    Add example secret file

---
+++ polar_auth/secret_example.py
@@ -0,0 +1,4 @@
+SECRET_KEY = 'A random string of characters'
+polar_key = "Polar project key"
+polar_secret = "Polar project secret token"
+data_server_key = "RSA key authorized to access data server"
